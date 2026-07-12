56d9353a398206af02c513374f1cf0a447b5cecb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "data_factory_dataset_https" {
-  description = "All data_factory_dataset_http resources"
-  value       = azurerm_data_factory_dataset_http.data_factory_dataset_https
-}
 output "data_factory_dataset_https_additional_properties" {
-  description = "List of additional_properties values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.additional_properties }
 }
 output "data_factory_dataset_https_annotations" {
-  description = "List of annotations values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.annotations]
+  description = "Map of annotations values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.annotations }
 }
 output "data_factory_dataset_https_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.data_factory_id }
 }
 output "data_factory_dataset_https_description" {
-  description = "List of description values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.description]
+  description = "Map of description values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.description }
 }
 output "data_factory_dataset_https_folder" {
-  description = "List of folder values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.folder]
+  description = "Map of folder values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.folder }
 }
 output "data_factory_dataset_https_linked_service_name" {
-  description = "List of linked_service_name values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.linked_service_name]
+  description = "Map of linked_service_name values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.linked_service_name }
 }
 output "data_factory_dataset_https_name" {
-  description = "List of name values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.name]
+  description = "Map of name values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.name }
 }
 output "data_factory_dataset_https_parameters" {
-  description = "List of parameters values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.parameters]
+  description = "Map of parameters values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.parameters }
 }
 output "data_factory_dataset_https_relative_url" {
-  description = "List of relative_url values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.relative_url]
+  description = "Map of relative_url values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.relative_url }
 }
 output "data_factory_dataset_https_request_body" {
-  description = "List of request_body values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.request_body]
+  description = "Map of request_body values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.request_body }
 }
 output "data_factory_dataset_https_request_method" {
-  description = "List of request_method values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.request_method]
+  description = "Map of request_method values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.request_method }
 }
 output "data_factory_dataset_https_schema_column" {
-  description = "List of schema_column values across all data_factory_dataset_https"
-  value       = [for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : v.schema_column]
+  description = "Map of schema_column values across all data_factory_dataset_https, keyed the same as var.data_factory_dataset_https"
+  value       = { for k, v in azurerm_data_factory_dataset_http.data_factory_dataset_https : k => v.schema_column }
 }
 
